aff98060ba34aa0e515fb65907c2e8f48ed7234a
Egor Gaydamak <mak.egor33@gmail.com>
2023-08-06 02:33:11 +0200

    Configure Demo Project

--- BasicAnalyticsDemo/BasicAnalyticsDemo/AppDelegate.swift
+++ BasicAnalyticsDemo/BasicAnalyticsDemo/AppDelegate.swift
@@ -1,10 +1,27 @@
 import UIKit
 
+import BasicAnalytics
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    var analytics: Analytics? = nil
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        analytics = Analytics(configuration: Configuration(writingKey: "DemoAppWritingKey"))
+        
         return true
     }
 }
 
+extension UIApplicationDelegate {
+    var analytics: Analytics? {
+        if let appDelegate = self as? AppDelegate {
+            return appDelegate.analytics
+        }
+        return nil
+    }
+}
--- BasicAnalyticsDemo/BasicAnalyticsDemo/ViewController.swift
+++ BasicAnalyticsDemo/BasicAnalyticsDemo/ViewController.swift
@@ -1,8 +1,62 @@
 import UIKit
 
+import BasicAnalytics
+
 class ViewController: UIViewController {
+    var analytics: Analytics? {
+        return UIApplication.shared.delegate?.analytics
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onStartTap(_ sender: Any) {
+        print("press start \(Date())")
+        
+        analytics?.startSession(completion: { result in
+            print("analytics?.startSession \(result)")
+        })
+    }
+    
+    @IBAction func onEvent1Tap(_ sender: Any) {
+        print("press 1 \(Date())")
+        
+        analytics?.track(event: .init(
+            name: "1",
+            properties: [
+                "bool": .boolean(true),
+                "string": .string("string")
+            ]
+        ))
+    }
+    
+    @IBAction func onEvent2Tap(_ sender: Any) {
+        print("press 2 \(Date())")
+        
+        analytics?.track(event: .init(name: "2"))
+    }
+    
+    @IBAction func onEvent3Tap(_ sender: Any) {
+        print("press 3 \(Date())")
+        
+        analytics?.track(event: .init(name: "3"))
+    }
+    
+    @IBAction func onEndTap(_ sender: Any) {
+        print("press end \(Date())")
+        
+        analytics?.endSession(completion: { result in
+            print("analytics?.endSession \(result)")
+        })
+    }
+    
+    @IBAction func onLastTap(_ sender: Any) {
+        print("press last \(Date())")
+        
+        analytics?.getLastSession(completion: { result in
+            print("analytics?.getLastSession \(result)")
+        })
     }
 }
